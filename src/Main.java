31b30494515ab4499947c97b92b07f3b4d8a8dca
Avoid <avoid87@bk.ru>
2023-09-11 22:15:03 +0300

    Homework_11/09/23_2

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,10 @@
 public class Main {
     public static void main(String[] args) {
 
-                Basket vasyaBasket = new Basket();
-                vasyaBasket.add("Молоко", 300);
-                vasyaBasket.add("Колбаса", 400);
+        Product product = new Product();
+        product.setName("Иван");
+        System.out.println(product.getName());
 
-                Basket mashaBasket = new Basket();
-                mashaBasket.add("Сосиски",600);
 
-                vasyaBasket.print("Корзина Васи: ");
-
-                mashaBasket.print("Корзина Маши: ");
-                mashaBasket.print("Basket");
     }
 }
